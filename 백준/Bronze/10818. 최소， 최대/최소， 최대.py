1f9613bbc76be96d62374ab2d0e2bbd9c0724838
Kevin shin <154401566+LinkDiscovery@users.noreply.github.com>
2024-03-13 10:01:10 +0900

    [Bronze III] Title: 최소, 최대, Time: 240 ms, Memory: 93704 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,4 +1,18 @@
-a= int(input())
-List = list(map(int,input().split()))
+import os
 
-print(min(List),max(List))
+
+def main():
+    tokens = map(int, iter(os.read(0, os.stat(0).st_size).split()))
+    next(tokens)
+    a = 1 << 29
+    b = -a
+    for x in tokens:
+        if x < a:
+            a = x
+        if x > b:
+            b = x
+    os.write(1, (str(a) + ' ' + str(b)).encode())
+    os._exit(0)
+
+
+main()
